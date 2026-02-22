0d3c05ad03858c5a7bd71481b1c35bcf5156082a
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-02-22 12:27:53 -0300

    botao logou com loading

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { useNavigate } from "react-router";
+import { Button } from "@/components/ui/button";
+
 export function DashboardPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      await supabase.auth.signOut();
+      navigate("/entrar");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <main className="w-full flex justify-center items-center">
       <h1>Em construção</h1>
+      <Button disabled={isLoading} onClick={handleLogout}>
+        {isLoading ? "Saindo..." : "Sair da conta"}
+      </Button>
     </main>
   );
 }

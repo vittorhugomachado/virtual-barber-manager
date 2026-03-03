1e641c7447cb02e17f83816c09c7493d3afc7b75
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-03 11:54:19 -0300

    corrigido sidebar com height maior

--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -41,7 +41,7 @@ export function SidebarComponent() {
   const { state, isMobile } = useSidebar();
 
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar collapsible="icon" className="h-screen">
       <SidebarHeader className="flex items-center justify-between">
         {state === "expanded" || isMobile ? (
           <Logo style="w-full px-8 pt-2 pb-4.5 m-2 mb-4 border-b" />

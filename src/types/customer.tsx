a963a411c561205a9df1ed0c9ec40b3b95d245ee
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-07 14:24:05 -0300

    criado tipos do customer

---
+++ src/types/customer.tsx
@@ -0,0 +1,10 @@
+export type Customer = {
+  id: string;
+  barbershop_id: string;
+  name: string;
+  phone: string;
+  created_at: string;
+  updated_at: string;
+  total_appointments?: number;
+  last_appointment?: string | null;
+};

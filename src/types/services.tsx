425bc60dcdf99f1997d3eaa1305ccbd3218481ee
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-07 12:00:25 -0300

    tipos do service criado

---
+++ src/types/services.tsx
@@ -0,0 +1,10 @@
+export type Service = {
+  id: string;
+  barbershop_id: string;
+  name: string;
+  description: string | null;
+  image_url: string | null;
+  duration_min: number | null;
+  price: number | null;
+  is_active: boolean;
+};

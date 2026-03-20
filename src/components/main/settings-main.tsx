80b29af281d995675642b310217d8906b3619d28
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-20 13:51:23 -0300

    criado configuração de endereço

--- src/components/main/settings-main.tsx
+++ src/components/main/settings-main.tsx
@@ -3,11 +3,14 @@ import { UsersSection } from "../sections/settings-page/user-section";
 // import { PlansSection } from "../sections/settings-page/plans-section";
 import { Separator } from "@/components/ui/separator";
 import { OpeningHoursSection } from "../sections/settings-page/opening-hours-section";
+import { AddressSection } from "../sections/settings-page/address-section";
 
 export function SettingsMain() {
   return (
     <div className="w-full flex flex-col items-center overflow-y-auto overflow-x-hidden">
       <SettingsForm />
+      <Separator className="my-4 max-w-180 px-6 md:px-16" />
+      <AddressSection />
       <Separator className="my-4 max-w-180 px-6 md:px-16" />
       <OpeningHoursSection />
       <Separator className="my-4 max-w-180 px-6 md:px-16" />

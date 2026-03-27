a69a72cccee65259336c639915fcd879036a9708
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-03-26 22:55:49 -0300

    client supabase com lazy

--- src/lib/supabase/lazy-supabase.ts
+++ src/lib/supabase/lazy-supabase.ts
@@ -1,17 +1,6 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
-
-let supabasePromise: Promise<SupabaseClient> | null = null;
+import { supabase } from "./supabase";
 
 export function getSupabaseClient() {
-  if (!supabasePromise) {
-    supabasePromise = import("@supabase/supabase-js").then(
-      ({ createClient }) =>
-        createClient(
-          import.meta.env.VITE_SUPABASE_URL!,
-          import.meta.env.VITE_SUPABASE_ANON_KEY!,
-        ),
-    );
-  }
-
-  return supabasePromise;
+  return Promise.resolve<SupabaseClient>(supabase);
 }

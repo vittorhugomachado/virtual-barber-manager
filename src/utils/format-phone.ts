db35fe7645cf9834dc271f7d01f89a91169dd39c
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-05-28 10:35:19 -0300

    corrigido erro format phone

--- src/utils/format-phone.ts
+++ src/utils/format-phone.ts
@@ -1,5 +1,6 @@
-export const formatPhone = (n: number) =>
-  n
-    .toString()
+export const formatPhone = (n: string | number | undefined | null): string => {
+  if (n == null) return "";
+  return String(n)
     .replace(/\D/g, "")
     .replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3");
+};
